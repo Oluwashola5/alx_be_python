13736e10cd6449008ac8299d8acaf2c5475938d1
Oluwashola5 <oluwasholabj@gmail.com>
2024-12-11 22:15:41 +0100

    Giving ALx checker what it wants

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -2,6 +2,6 @@ principal = 1000
 rate = 0.05
 time = 3
 
-Interest = (principal * rate * time)
+interest = principal * rate * time
 
-print ("The simple interest is:", (principal * rate * time))
+print ("The simple interest is:", (interest))
