c7e692143fd9f614fa1a901583fc34b78c3bb2e2
Oluwashola5 <oluwasholabj@gmail.com>
2024-12-11 21:22:39 +0100

    add code for subtration and multiplication

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,8 +1,6 @@
-int number1 = 10
-int number2 = 5
+number1 = 10
+number2 = 5
 
-addition = +
-subtraction = -
-multiplication = *
-
-print ("Addition of" number1 and number2 is (number1 + number2))
+print ("Addition of", number1, "and", number2, "is", (number1 + number2))
+print ("Subtraction", number1, "and", number2, "is", (number1 - number2))
+print ("Multiplication", number1, "and", number2, "is", (number1 * number2))
